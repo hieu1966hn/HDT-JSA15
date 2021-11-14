9f9326c81b2e2069c729809a5b27ff4d7645d2d0
hieu1966hn <hieu1966hn@gmail.com>
2021-11-14 16:50:51 +0700

    done SS4

--- SS4/Ôn tập JS/study.js
+++ SS4/Ôn tập JS/study.js
@@ -24,24 +24,46 @@
 
 
 // khai báo mảng (dùng để: lưu trữ nhiều kiểu dữ liệu bên trong dưới dạng danh sách )
-let animals = [];
+// let animals = [];
 
-////// C,R,U,D với mảng.
+// ////// C,R,U,D với mảng.
 
-// Create: thêm 1 phần tử vào mảng: push('dfsa')
-animals.push('dog');
-animals.push('cat');
-
-
-// R: Read: đọc mảng
-console.log(animals);
+// // Create: thêm 1 phần tử vào mảng: push('dfsa')
+// animals.push('dog');
+// animals.push('cat');
 
 
-// U: Update: cập nhật, sửa đổi phần tử trong mảng
-animals[1] = "fish"
-console.log("mảng sau khi update là", animals);
+// // R: Read: đọc mảng
+// console.log(animals);
 
-// D: Delete: xóa 1 hoặc nhiều phần tử trong mảng
-animals.splice(0, 1);
-console.log("mảng sau khi Delete vị trí 0 là", animals);
 
+// // U: Update: cập nhật, sửa đổi phần tử trong mảng
+// animals[1] = "fish"
+// console.log("mảng sau khi update là", animals);
+
+// // D: Delete: xóa 1 hoặc nhiều phần tử trong mảng
+// animals.splice(0, 1);
+// console.log("mảng sau khi Delete vị trí 0 là", animals);
+
+////////// KHai báo đối tượng (Khi cần lưu trữ thong tin dưới dạng từ khóa)
+
+let Human = {
+  name: "Hiếu",
+  age: 24,
+  address: "Hà Nội",
+  cmt: "0123456789",
+  vaccine: "Đã tiêm 2 mũi",
+}
+
+console.log(Human.cmt);
+
+// so sánh với mảng:
+let HumanArray = [
+  "Hiếu",
+  24,
+  "Hà Nội",
+  "0123456789",
+  "Đã tiêm 2 mũi",
+]
+
+console.log(Human.cmt);
